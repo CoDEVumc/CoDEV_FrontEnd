5bf44a2c3df544a88d7280c4555d92bd9a6bd9ca
Lee-hwansoo <lee_hwansoo@naver.com>
2023-01-14 23:27:44 +0900

    Feat : 로그인과 회원가입 레이아웃

--- app/src/main/java/com/example/codev/MainActivity.kt
+++ app/src/main/java/com/example/codev/MainActivity.kt
@@ -2,9 +2,6 @@ package com.example.codev
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.codev.databinding.ActivityMainBinding
 
@@ -17,14 +14,9 @@ class MainActivity : AppCompatActivity() {
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
-        //가운데 정렬 글 작성 예시
-        viewBinding.toolbarLogin.toolbar2.title = ""
-        viewBinding.toolbarLogin.toolbarText.text = "툴바의 가운데 정렬 글 작성 예시"
-
-        setSupportActionBar(viewBinding.toolbarLogin.toolbar2)
-        supportActionBar?.apply {
-            setDisplayHomeAsUpEnabled(true)
-            setHomeAsUpIndicator(R.drawable.left2)
+        viewBinding.btnRegister.setOnClickListener {
+            val intent = Intent(this,RegisterTosActivity::class.java)
+            startActivity(intent)
         }
 
         viewBinding.btnLogin.setOnClickListener {
@@ -32,28 +24,5 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
-    }
-
-    //모집글 상세페이지의 삭제하기 수정하기 예시
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        // Inflate the menu, this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_toolbar_detail, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId){
-            android.R.id.home ->{
-                Toast.makeText(this, "뒤로가기",Toast.LENGTH_SHORT).show()
-                //finish()
-            }
-            R.id.menu_modify ->{
-                Toast.makeText(this, "수정하기",Toast.LENGTH_SHORT).show()
-            }
-            R.id.menu_delete ->{
-                Toast.makeText(this, "삭제하기",Toast.LENGTH_SHORT).show()
-            }
-        }
-        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/example/codev/RegisterTypeActivity.kt
@@ -0,0 +1,42 @@
+package com.example.codev
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import com.example.codev.databinding.ActivityRegisterTypeBinding
+
+class RegisterTypeActivity:AppCompatActivity() {
+    private lateinit var viewBinding: ActivityRegisterTypeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding = ActivityRegisterTypeBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        viewBinding.toolbarRegister.toolbar2.title = ""
+
+        setSupportActionBar(viewBinding.toolbarRegister.toolbar2)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.left2)
+        }
+
+        viewBinding.btnRegisterNext.setOnClickListener {
+            val intent = Intent(this,RegisterEmailActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            android.R.id.home ->{
+                Toast.makeText(this, "뒤로가기",Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
