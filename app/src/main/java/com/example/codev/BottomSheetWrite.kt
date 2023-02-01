f77c9755e9d9876c108d12a19c7b94d5cc0c9824
sujinyang00 <sujinyang00@gmail.com>
2023-02-01 23:06:23 +0900

    작성하기 버튼 연결 완료

--- app/src/main/java/com/example/codev/BottomSheetWrite.kt
+++ app/src/main/java/com/example/codev/BottomSheetWrite.kt
@@ -1,11 +1,14 @@
 package com.example.codev
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.example.codev.addpage.AddNewProjectActivity
+import com.example.codev.addpage.AddNewStudyActivity
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.example.codev.databinding.PopupWriteBinding
 
@@ -28,11 +31,17 @@ class BottomSheetWrite(private val returnWrite: (String) -> Unit) : BottomSheetD
                     write = "project"
                     //Log.d("test: ","최신순")
                     Toast.makeText(this.context, "프로젝트 버튼 클릭!", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(getActivity(), AddNewProjectActivity::class.java)
+                    startActivity(intent)
+                    popupWriteBinding.radioGroupWrite.clearCheck()
                 }
                 R.id.btn_write_study -> {
                     write = "study"
                     //Log.d("test: ","추천순")
                     Toast.makeText(this.context, "스터디 버튼 클릭!", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(getActivity(), AddNewStudyActivity::class.java)
+                    startActivity(intent)
+                    popupWriteBinding.radioGroupWrite.clearCheck()
                 }
             }
 
@@ -41,13 +50,12 @@ class BottomSheetWrite(private val returnWrite: (String) -> Unit) : BottomSheetD
 
 
 
-
         return popupWriteBinding.root
-        //return inflater.inflate(R.layout.popup_loc, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
 
         popupWriteBinding.btnWriteProject.setOnClickListener {
             dismiss()
