c0bb9c46b9faff6a2c2eb12487e480e58adba6d4
Lee-hwansoo <lee_hwansoo@naver.com>
2023-02-06 18:55:57 +0900

    [Feat] : 지원동기 500자 제한

--- app/src/main/java/com/example/codev/RecruitApplyActivity.kt
+++ app/src/main/java/com/example/codev/RecruitApplyActivity.kt
@@ -93,7 +93,11 @@ class RecruitApplyActivity:AppCompatActivity() {
         viewBinding.etContent.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
-            override fun afterTextChanged(s: Editable?) {}
+            override fun afterTextChanged(s: Editable?) {
+                if (s != null) {
+                    viewBinding.contentTextCounter.text = s.length.toString() + "/500"
+                }
+            }
         })
 
         viewBinding.submitButton.setOnClickListener {
