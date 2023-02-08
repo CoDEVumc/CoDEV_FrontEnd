9a29a948d9aebbe740d79cfbf4ac5e23488394c9
newoldname <thjbjs@outlook.com>
2023-02-08 20:16:20 +0900

    [fix] 홈페이지에서 모집중인 글만 나오게 함.

--- app/src/main/java/com/example/codev/HomeFragment.kt
+++ app/src/main/java/com/example/codev/HomeFragment.kt
@@ -50,7 +50,7 @@ class HomeFragment():Fragment() {
 
 
     private fun loadData(context: Context){
-        var tempBannerList = ArrayList<HomeBannerItem>()
+        val tempBannerList = ArrayList<HomeBannerItem>()
         tempBannerList.add(HomeBannerItem("http://semtle.catholic.ac.kr:8080/image?name=banner120230207154253.png", "banner1"))
         tempBannerList.add(HomeBannerItem("http://semtle.catholic.ac.kr:8080/image?name=banner220230207154253.png", "banner2"))
         tempBannerList.add(HomeBannerItem("http://semtle.catholic.ac.kr:8080/image?name=banner320230207154253.png", "banner3"))
@@ -61,21 +61,21 @@ class HomeFragment():Fragment() {
         setProjectAdapter()
         setStudyAdapter()
 
-        var tempPostList = ArrayList<HomeCommunityItem>()
+        val tempPostList = ArrayList<HomeCommunityItem>()
         tempPostList.add(HomeCommunityItem("현직자가 알려주는 면접 단골 질문 \uD83D\uDC68\uD83C\uDFFB\u200D\uD83D\uDCBB", "http://semtle.catholic.ac.kr:8080/image?name=post120230207155427.jpg", "02/04 20:45", 225, 3, 56))
         tempPostList.add(HomeCommunityItem("\uD83D\uDD0E 개발자를 위한 정보 검색 팁! ", null, "02/04 21:51", 100, 3, 45))
         tempPostList.add(HomeCommunityItem("\uD83D\uDD25 UI/UX 디자이너 희망자라면 꼭 봐야 할 꿀팁!", "http://semtle.catholic.ac.kr:8080/image?name=post220230207155427.jpg", "02/04 20:01", 103, 7, 56))
         tempPostList.add(HomeCommunityItem("2023 블록체인 시리즈 세미나 \uD83C\uDF10", null, "02/04 20:00", 223, 2, 40))
         setPostView(tempPostList)
 
-        var tempQnaList = ArrayList<HomeCommunityItem>()
+        val tempQnaList = ArrayList<HomeCommunityItem>()
         tempQnaList.add(HomeCommunityItem("다들 개발 공부 어떤 식으로 하시나요?", null, "02/04 21:51", 2, 11, 5))
         tempQnaList.add(HomeCommunityItem("이 에러 어떻게 해결하나요?", "http://semtle.catholic.ac.kr:8080/image?name=post320230207155427.jpg", "02/04 20:45", 1, 10, 5))
         tempQnaList.add(HomeCommunityItem("개발자 취업 포트폴리오 어떻게 쌓나요?", "http://semtle.catholic.ac.kr:8080/image?name=post420230207155427.jpg", "02/04 20:01", 5, 16, 7))
         tempQnaList.add(HomeCommunityItem("취업 시 학점 많이 중요한가요?", null, "02/04 20:00", 7, 7, 16))
         setQnaView(tempQnaList)
 
-        var tempGameList = ArrayList<HomeGameItem>()
+        val tempGameList = ArrayList<HomeGameItem>()
         tempGameList.add( HomeGameItem("http://semtle.catholic.ac.kr:8080/image?name=120230207155427.jpg", "2023 야간관광 특화도시 숏폼 공모전", "한국관광공사"))
         tempGameList.add( HomeGameItem("http://semtle.catholic.ac.kr:8080/image?name=220230207155427.png", "2023 미래도서관 정책 아이디어 해커톤 대회", "국가도서관위원회"))
         tempGameList.add( HomeGameItem("http://semtle.catholic.ac.kr:8080/image?name=320230207155427.jpg", "2023 제 1 회 CCC 커튼콜 마케팅 공모전", "라이브콘테스트"))
@@ -128,7 +128,7 @@ class HomeFragment():Fragment() {
 
     private fun setProjectAdapter(){
         RetrofitClient.service.requestPDataList(AndroidKeyStoreUtil.decrypt(UserSharedPreferences.getUserAccessToken(mainAppActivity)),
-            0, "", "", "", "", "populaRity").enqueue(object: Callback<ResGetProjectList> {
+            0, "", "", "", "ING", "populaRity").enqueue(object: Callback<ResGetProjectList> {
                 override fun onResponse(call: Call<ResGetProjectList>, response: Response<ResGetProjectList>) {
                     if(response.isSuccessful.not()){
                         Log.d("test: 조회실패",response.toString())
@@ -168,7 +168,7 @@ class HomeFragment():Fragment() {
 
     private fun setStudyAdapter(){
         RetrofitClient.service.requestSDataList(AndroidKeyStoreUtil.decrypt(UserSharedPreferences.getUserAccessToken(mainAppActivity)),
-            0, "", "", "", "", "populaRity").enqueue(object: Callback<ResGetStudyList> {
+            0, "", "", "", "ING", "populaRity").enqueue(object: Callback<ResGetStudyList> {
             override fun onResponse(call: Call<ResGetStudyList>, response: Response<ResGetStudyList>) {
                 if(response.isSuccessful.not()){
                     Log.d("test: 조회실패",response.toString())
