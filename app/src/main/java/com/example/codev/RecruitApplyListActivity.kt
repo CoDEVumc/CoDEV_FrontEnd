14bbd73989f60333b43664c02d147da7a4824a01
sujinyang00 <sujinyang00@gmail.com>
2023-02-15 03:51:51 +0900

    모집완료 수정 끝

--- app/src/main/java/com/example/codev/RecruitApplyListActivity.kt
+++ app/src/main/java/com/example/codev/RecruitApplyListActivity.kt
@@ -287,12 +287,12 @@ class RecruitApplyListActivity: AppCompatActivity() {
                             200 -> {
                                 response.body()?.let {
                                     Log.d("test: 지원자 리스트 불러오기 성공", "\n${it.toString()}")
-                                    Log.d("test: 지원자 리스트 불러오기 성공", "\n${it.result.message.co_appllicantsInfo}")
+                                    Log.d("test: 지원자 리스트 불러오기 성공", "\n${it.result.message.co_applicantsInfo}")
                                     setAdapter1(it.result.message.co_applicantsCount, context, limit)
-                                    setAdapter2(it.result.message.co_appllicantsInfo, context)
+                                    setAdapter2(it.result.message.co_applicantsInfo, context)
 
                                     applicantList= ArrayList()
-                                    for (i in it.result.message.co_appllicantsInfo){
+                                    for (i in it.result.message.co_applicantsInfo){
                                         applicantList.add(i.co_email)
                                     }
 
@@ -346,7 +346,7 @@ class RecruitApplyListActivity: AppCompatActivity() {
                                         viewBinding.bottomBtn.isGone = true
                                         viewBinding.btnSelect1.isGone = true
                                         viewBinding.btnSelect2.isGone = true
-                                        peopleNum = it.result.message.co_appllicantsInfo.size
+                                        peopleNum = it.result.message.co_applicantsInfo.size
                                         viewBinding.applicantNum.text = "현재 파트 지원자 $peopleNum"
                                     }
                                 }
@@ -373,12 +373,12 @@ class RecruitApplyListActivity: AppCompatActivity() {
                             200 -> {
                                 response.body()?.let {
                                     Log.d("test: 지원자 리스트 불러오기 성공", "\n${it.toString()}")
-                                    Log.d("test: 지원자 리스트 불러오기 성공", "\n${it.result.message.co_appllicantsInfo}")
+                                    Log.d("test: 지원자 리스트 불러오기 성공", "\n${it.result.message.co_applicantsInfo}")
                                     setAdapter1(it.result.message.co_applicantsCount, context, limit)
 
                                     var temp = ArrayList<ApplicantInfoData>()
-                                    if(it.result.message.co_appllicantsInfo != null){
-                                        temp = it.result.message.co_appllicantsInfo
+                                    if(it.result.message.co_applicantsInfo != null){
+                                        temp = it.result.message.co_applicantsInfo
                                     }
                                     setAdapter2(temp, context)
 
--- app/src/main/java/com/example/codev/ResApplyerList.kt
+++ app/src/main/java/com/example/codev/ResApplyerList.kt
@@ -15,7 +15,7 @@ data class ApplyerData(
     val co_part: String,
     val co_tempSavedApplicantsCount: Int, //선택한 지원자 전체 수 (헤더의 getItemCount)
     val co_applicantsCount: ArrayList<ApplicantData>, //파트별 지원자 수 (토글 정보)
-    val co_appllicantsInfo: ArrayList<ApplicantInfoData> //파트별 지원자 정보 (바디 정보 :프로필+포트폴리오)
+    val co_applicantsInfo: ArrayList<ApplicantInfoData> //파트별 지원자 정보 (바디 정보 :프로필+포트폴리오)
 )
 data class ApplicantData( //파트별 지원자 수 (토글 정보)
     val co_part: String,
