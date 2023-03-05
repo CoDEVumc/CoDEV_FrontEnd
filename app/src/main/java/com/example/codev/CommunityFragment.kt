98c2f98e87fbf86dc564165600e0a22d455da44e
sujinyang00 <sujinyang00@gmail.com>
2023-03-05 19:37:48 +0900

    values/infomation.xml 삭제

--- app/src/main/java/com/example/codev/CommunityFragment.kt
+++ app/src/main/java/com/example/codev/CommunityFragment.kt
@@ -1,6 +1,7 @@
 package com.example.codev
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -25,9 +26,14 @@ class CommunityFragment:Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         viewBinding = FragmentCommunityBinding.inflate(layoutInflater)
-        viewBinding.toolbarCommunity.toolbar1.inflateMenu(R.menu.menu_toolbar_1)
+        var temp = viewBinding.toolbarCommunity.toolbar1
+        viewBinding.toolbarCommunity.toolbar1.inflateMenu(R.menu.menu_toolbar_4)
         viewBinding.toolbarCommunity.toolbar1.title = ""
         viewBinding.toolbarCommunity.toolbarImg.setImageResource(R.drawable.logo_community)
+
+        val intent = Intent(mainAppActivity, CommunityActivity::class.java)
+        startActivity(intent)
+
 
         return viewBinding.root
     }
