7b89c02bcd3131323a782791c95f49dcb6c87c29
sujinyang00 <sujinyang00@gmail.com>
2023-03-26 03:23:04 +0900

    Feat: 커뮤니티 서버랑 연결 완료

--- app/src/main/java/com/example/codev/CommunityInfoFragment.kt
+++ app/src/main/java/com/example/codev/CommunityInfoFragment.kt
@@ -8,10 +8,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.example.codev.databinding.ActivityMyBookmarkBinding
-import com.example.codev.databinding.FragmentCommunityInfoBinding
-import com.example.codev.databinding.FragmentCommunityQuestionBinding
-import com.example.codev.databinding.FragmentMyBookmarkProjectBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.codev.databinding.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -20,11 +19,30 @@ import java.util.ArrayList
 class CommunityInfoFragment :Fragment(){
     private lateinit var viewBinding: FragmentCommunityInfoBinding
 
-    private lateinit var adapterPData: AdapterCommunityInfoList
+    private lateinit var adapterIData: AdapterCommunityInfoList
+
+    private var idataList: ArrayList<QIData> = ArrayList()
+
+    private var coMyBoard:Boolean = false //초기값 false
+
+    private var downpage: Int = 0
+    private var lastPage: Boolean = false
+
+    private lateinit var mainAppActivity: Context
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if(context is MainAppActivity){
+            mainAppActivity = context
+        }
+    }
 
     override fun onResume() {
         super.onResume()
-        //loadPData(requireContext())
+
+        idataList = ArrayList() //초기화
+
+        loadData(mainAppActivity, downpage, coMyBoard) //기본으로 0page PData 가져오기
+
     }
 
     override fun onCreateView(
@@ -34,37 +52,79 @@ class CommunityInfoFragment :Fragment(){
     ): View? {
         viewBinding = FragmentCommunityInfoBinding.inflate(layoutInflater)
 
+
+        //자동페이징 처리 부분
+        viewBinding.listviewInfo.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                val lastVisibleItemPosition = (recyclerView.layoutManager as LinearLayoutManager?)!!.findLastCompletelyVisibleItemPosition() - 2
+                val lastPosition = recyclerView.adapter!!.itemCount - 3 //원래 1
+
+                if((lastVisibleItemPosition == lastPosition) && !lastPage){ //처음에 false
+                    downpage += 1
+                    loadData(mainAppActivity, downpage, coMyBoard)
+                }
+            }
+        })
+
         return viewBinding.root
     }
 
-//    private fun loadPData(context: Context){
-//        RetrofitClient.service.getHeartedProject(AndroidKeyStoreUtil.decrypt(UserSharedPreferences.getUserAccessToken(context))).enqueue(object: Callback<ResBookMarkProjectList> {
-//            override fun onResponse(call: Call<ResBookMarkProjectList>, response: Response<ResBookMarkProjectList>) {
-//                if(response.isSuccessful.not()){
-//                    Log.d("test: 북마크 프로젝트 불러오기 실패",response.toString())
-//                    Toast.makeText(context, "서버와 연결을 시도했으나 실패했습니다.", Toast.LENGTH_SHORT).show()
-//                }
-//                when(response.code()){
-//                    200 -> {
-//                        response.body()?.let {
-//                            Log.d("test: 찜한 프로젝트 불러오기 성공", "\n${it.toString()}")
-//                            setPAdapter(it.result.Complete, context)
-//                        }
-//                    }
-//                }
-//            }
-//
-//            override fun onFailure(call: Call<ResBookMarkProjectList>, t: Throwable) {
-//                Log.d("test", "[Fail]${t.toString()}")
-//            }
-//        })
-//    }
-//
-//    private fun setPAdapter(projectList: ArrayList<BookmarkPData>, context: Context){
-//        if(projectList.size != 0) {
-//            adapterPData = AdapterMyBookmarkProjectList(context, projectList)
-//            viewBinding.listviewProject.adapter = adapterPData
-//        }
-//    }
+    //전체 정보글 조회
+    private fun loadData(context: Context, int: Int, coMyBoard:Boolean) {
+        RetrofitClient.service.requestIDataList(AndroidKeyStoreUtil.decrypt(UserSharedPreferences.getUserAccessToken(mainAppActivity)),
+            int, coMyBoard).enqueue(object: Callback<ResGetCommunityList1>{
+            override fun onResponse(call: Call<ResGetCommunityList1>, response: Response<ResGetCommunityList1>) {
+                if(response.isSuccessful.not()){
+                    Log.d("test: 조회실패",response.toString())
+                    Toast.makeText(context, "서버와 연결을 시도했으나 실패했습니다.", Toast.LENGTH_SHORT).show()
+                }else{
+                    when(response.code()){
+                        200->{
+                            response.body()?.let {
+                                Log.d("test: 정보글 조회 성공! ", "\n${it.toString()}")
+                                Log.d("test: 정보글 데이터 : ", "\n${it.result.success}")
+                                Log.d("test: 매개변수: ",coMyBoard.toString())
+                                //페이지가 비어있으면
+                                if(it.result.success.toString() == "[]") {
+                                    //Log.d("test: success: ", "[] 라서 비어있어용")
+                                    Toast.makeText(context,"이 글의 끝입니다.",Toast.LENGTH_SHORT).show()
+                                    lastPage = true
+                                    if(int == 0) { //0page
+                                        // 1.페이지가 끝이라서 그 다음페이지 결과가 []인거 --> int != 0
+                                        // 2.필터링 결과가 아무것도 없는거 --> int == 0
+                                        setQAdapter(idataList) //projectAdapter
+                                    }
+                                }
+                                //페이지에 내용이 있으면
+                                else {
+                                    idataList.addAll(it.result.success)
+                                    if(int == 0) { //0page
+                                        setQAdapter(idataList) //projectAdapter
+                                    }
+                                    else{
+                                        viewBinding.listviewInfo.adapter!!.notifyDataSetChanged()
+                                        //viewBinding.listviewMain.adapter!!.notifyItemRangeInserted(downpage*10-1,10)
+                                    }
+                                }
+                            }
+
+                        }
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<ResGetCommunityList1>, t: Throwable) {
+                Log.d("test: 조회실패 - RPF > loadData_q(질문글 전체조회): ", "[Fail]${t.toString()}")
+                Toast.makeText(context, "서버와 연결을 시도했으나 실패했습니다.", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun setQAdapter(infoList: ArrayList<QIData>){
+        val adapter = AdapterCommunityInfoList(mainAppActivity,infoList)
+        viewBinding.listviewInfo.adapter = adapter
+    }
 
 }
--- app/src/main/java/com/example/codev/CommunityQuestionFragment.kt
+++ app/src/main/java/com/example/codev/CommunityQuestionFragment.kt
@@ -8,6 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.codev.databinding.ActivityMyBookmarkBinding
 import com.example.codev.databinding.FragmentCommunityQuestionBinding
 import com.example.codev.databinding.FragmentMyBookmarkProjectBinding
@@ -19,11 +21,30 @@ import java.util.ArrayList
 class CommunityQuestionFragment :Fragment(){
     private lateinit var viewBinding: FragmentCommunityQuestionBinding
 
-    private lateinit var adapterPData: AdapterCommunityQuestionList
+    private lateinit var adapterQData: AdapterCommunityQuestionList
+
+    private var qdataList: ArrayList<QIData> = ArrayList()
+
+    private var coMyBoard:Boolean = false //초기값 false
+
+    private var downpage: Int = 0
+    private var lastPage: Boolean = false
+
+    private lateinit var mainAppActivity: Context
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if(context is MainAppActivity){
+            mainAppActivity = context
+        }
+    }
 
     override fun onResume() {
         super.onResume()
-        //loadPData(requireContext())
+
+        qdataList = ArrayList() //초기화
+
+        loadData(mainAppActivity, downpage, coMyBoard) //기본으로 0page PData 가져오기
+
     }
 
     override fun onCreateView(
@@ -33,37 +54,79 @@ class CommunityQuestionFragment :Fragment(){
     ): View? {
         viewBinding = FragmentCommunityQuestionBinding.inflate(layoutInflater)
 
+
+        //자동페이징 처리 부분
+        viewBinding.listviewQuestion.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                val lastVisibleItemPosition = (recyclerView.layoutManager as LinearLayoutManager?)!!.findLastCompletelyVisibleItemPosition() - 2
+                val lastPosition = recyclerView.adapter!!.itemCount - 3 //원래 1
+
+                if((lastVisibleItemPosition == lastPosition) && !lastPage){ //처음에 false
+                    downpage += 1
+                    loadData(mainAppActivity, downpage, coMyBoard)
+                }
+            }
+        })
+
         return viewBinding.root
     }
 
-//    private fun loadPData(context: Context){
-//        RetrofitClient.service.getHeartedProject(AndroidKeyStoreUtil.decrypt(UserSharedPreferences.getUserAccessToken(context))).enqueue(object: Callback<ResBookMarkProjectList> {
-//            override fun onResponse(call: Call<ResBookMarkProjectList>, response: Response<ResBookMarkProjectList>) {
-//                if(response.isSuccessful.not()){
-//                    Log.d("test: 북마크 프로젝트 불러오기 실패",response.toString())
-//                    Toast.makeText(context, "서버와 연결을 시도했으나 실패했습니다.", Toast.LENGTH_SHORT).show()
-//                }
-//                when(response.code()){
-//                    200 -> {
-//                        response.body()?.let {
-//                            Log.d("test: 찜한 프로젝트 불러오기 성공", "\n${it.toString()}")
-//                            setPAdapter(it.result.Complete, context)
-//                        }
-//                    }
-//                }
-//            }
-//
-//            override fun onFailure(call: Call<ResBookMarkProjectList>, t: Throwable) {
-//                Log.d("test", "[Fail]${t.toString()}")
-//            }
-//        })
-//    }
-//
-//    private fun setPAdapter(projectList: ArrayList<BookmarkPData>, context: Context){
-//        if(projectList.size != 0) {
-//            adapterPData = AdapterCommunityQuestionList(context, projectList)
-//            viewBinding.listviewProject.adapter = adapterPData
-//        }
-//    }
+    //전체 질문글 조회
+    private fun loadData(context: Context, int: Int, coMyBoard:Boolean) {
+        RetrofitClient.service.requestQDataList(AndroidKeyStoreUtil.decrypt(UserSharedPreferences.getUserAccessToken(mainAppActivity)),
+            int, coMyBoard).enqueue(object: Callback<ResGetCommunityList1>{
+            override fun onResponse(call: Call<ResGetCommunityList1>, response: Response<ResGetCommunityList1>) {
+                if(response.isSuccessful.not()){
+                    Log.d("test: 조회실패",response.toString())
+                    Toast.makeText(context, "서버와 연결을 시도했으나 실패했습니다.", Toast.LENGTH_SHORT).show()
+                }else{
+                    when(response.code()){
+                        200->{
+                            response.body()?.let {
+                                Log.d("test: 질문글 조회 성공! ", "\n${it.toString()}")
+                                Log.d("test: 질문글 데이터 : ", "\n${it.result.success}")
+                                Log.d("test: 매개변수: ",coMyBoard.toString())
+                                //페이지가 비어있으면
+                                if(it.result.success.toString() == "[]") {
+                                    //Log.d("test: success: ", "[] 라서 비어있어용")
+                                    Toast.makeText(context,"이 글의 끝입니다.",Toast.LENGTH_SHORT).show()
+                                    lastPage = true
+                                    if(int == 0) { //0page
+                                        // 1.페이지가 끝이라서 그 다음페이지 결과가 []인거 --> int != 0
+                                        // 2.필터링 결과가 아무것도 없는거 --> int == 0
+                                        setQAdapter(qdataList) //projectAdapter
+                                    }
+                                }
+                                //페이지에 내용이 있으면
+                                else {
+                                    qdataList.addAll(it.result.success)
+                                    if(int == 0) { //0page
+                                        setQAdapter(qdataList) //projectAdapter
+                                    }
+                                    else{
+                                        viewBinding.listviewQuestion.adapter!!.notifyDataSetChanged()
+                                        //viewBinding.listviewMain.adapter!!.notifyItemRangeInserted(downpage*10-1,10)
+                                    }
+                                }
+                            }
+
+                        }
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<ResGetCommunityList1>, t: Throwable) {
+                Log.d("test: 조회실패 - RPF > loadData_q(질문글 전체조회): ", "[Fail]${t.toString()}")
+                Toast.makeText(context, "서버와 연결을 시도했으나 실패했습니다.", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun setQAdapter(questionList: ArrayList<QIData>){
+        val adapter = AdapterCommunityQuestionList(mainAppActivity,questionList)
+        viewBinding.listviewQuestion.adapter = adapter
+    }
 
 }
